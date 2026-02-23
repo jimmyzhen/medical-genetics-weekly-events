591b2caf68c2835b693cb08eb36dcf8c01409b83
Jimmy Zhen <jzhen@stanford.edu>
2026-02-22 23:49:52 -0800

    Inline EMAIL_CSS with juice.inlineContent
    Remove the embedded <style> block from wrapEmailDocument and switch from juice(fullDocument) to juice.inlineContent(fullDocument, EMAIL_CSS). The change inlines the EMAIL_CSS correctly, then reinserts the CSS along with MSO table/td fixes into the head for better email client compatibility.

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -183,9 +183,6 @@ function wrapEmailDocument(bodyHtml, subject) {
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/fontawesome.min.css" integrity="sha512-SgaqKKxJDQ/tAUAAXzvxZz33rmn7leYDYfBP+YoMRSENhf3zJyx3SBASt/OfeQwBHA1nxMis7mM3EV/oYT6Fdw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/solid.min.css" integrity="sha512-yDUXOUWwbHH4ggxueDnC5vJv4tmfySpVdIcN1LksGZi8W8EVZv4uKGrQc0pVf66zS7LDhFJM7Zdeow1sw1/8Jw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <!--<![endif]-->
-<style type="text/css">
-${EMAIL_CSS}
-</style>
 </head>
 <body bgcolor="#eeeeee" class="em_body" style="margin:0px auto; padding:0px; background-color:#eeeeee;">
 ${bodyHtml}
@@ -211,9 +208,10 @@ export const handler = async (event) => {
     }
 
     const fullDocument = wrapEmailDocument(html, subject);
-    const inlinedHtml = juice(fullDocument).replace(
+    const inlinedHtml = juice.inlineContent(fullDocument, EMAIL_CSS).replace(
         '</head>',
         `<style type="text/css">
+${EMAIL_CSS}
 table { mso-table-lspace: 0; mso-table-rspace: 0; }
 td, a, span { mso-line-height-rule: exactly; }
 </style>
